fd5335fd11566d7620878cecc4c5039443c1281f
Kuziv Alexander <makklays@gmail.com>
2017-05-03 15:16:32 +0300

    Create lib.php

---
+++ namespace/lib/lib.php
@@ -0,0 +1,23 @@
+<?php
+namespace Lib;
+
+class A
+{
+    public $title = '';
+
+    public function setTitle($t)
+    {
+        $this->title = $t;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
+
+function ff()
+{
+    return 'fffffffff';
+}
+
