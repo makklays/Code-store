53d4021ae3ea6fee5fc14fc1b45bfd123586484d
makklays <makklays@gmail.com>
2017-11-27 11:26:50 +0200

    Create index.php
    HTML форма для загрузки файлов

---
+++ upload_files/index.php
@@ -0,0 +1,10 @@
+<?php
+// Загрузка файлов
+?>
+
+<script type="text/javascript" async="true" src="jquery-3.2.1.js"></script>
+<script type="text/javascript" async="true" src="upload.js"></script>
+
+<input type="file" multiple="multiple" accept=".txt,image/*" />
+<a href="#" class="submit button">Загрузить файлы</a>
+<div class="ajax-respond"></div>
