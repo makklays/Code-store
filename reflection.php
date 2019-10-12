b8c3b710a3c548551f1eea4a6fcf48316943d563
Alexander Kuziv <makklays@gmail.com>
2019-10-12 19:27:14 +0300

    Update reflection.php

--- reflection.php
+++ reflection.php
@@ -13,7 +13,29 @@
 $obj = new $className();
 $obj->methodName();
 
+// reflection function
 $funcReflactor = new ReflectionFunction('func_name');
 
+echo $funcReflactor->getFileName();
+echo $funcReflactor->getStartLine();
+echo $funcReflactor->getEndLine();
+echo $funcReflactor->getDocComment();
+
+// reflection class
 $classReflector = new ReflectionClass('class_name');
 
+// reflection object
+$blogs = Blog::getById($id);
+
+$objectReflector = new ReflectionObject($blogs);
+
+$properties = $objectReflector->getProperties();
+var_dump($properties);
+
+foreach($properties as $prop){
+    echo $prop->getName();
+    echo $prop->getMethods();
+    echo $prop->getConstants();
+    echo $prop->newInstance();
+    echo $prop->newInstanceWithoutConstructor();
+}
