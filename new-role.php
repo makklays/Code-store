5a40f219b5a7f781bf95b5fb39929aef48e6f6fc
Alexander Kuziv <makklays@gmail.com>
2021-08-06 08:15:21 +0200

    Create new-role.php
    Looking for a new job Developer PHP

---
+++ new-role.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * E-mail: makklays@gmail.com
+ * Description: Looking for a new job Developer PHP
+ **/
+
+function newRole()
+{
+    return {
+        'position': 'Developer PHP',
+        'salary_min': '1500',
+        'salary_max': '4500',
+        'salary_currency': 'USD',
+        'location': 'office',
+        'notify': 'sms,email,phone'
+    }
+}
+?>
