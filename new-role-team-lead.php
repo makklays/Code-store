6efb7fa27d38991198eccdaab22b7f8037d69429
Alexander Kuziv <makklays@gmail.com>
2021-08-16 22:29:58 +0200

    Team Lead

--- new-role-team-lead.php
+++ new-role-team-lead.php
@@ -1,1 +1,19 @@
+<?php
+/**
+ * Author: Alexander Kuziv 
+ * E-mail: makklays@gmail.com
+ * Description: Looking for a new job Team Lead
+ **/
 
+function newRoleTeamLead()
+{
+    return {
+        'position': 'Team Lead',
+        'salary_min': '2500',
+        'salary_max': '5000',
+        'salary_currency': 'USD',
+        'location': 'office',
+        'notify': 'sms,email,phone'
+    }
+}
+?>
