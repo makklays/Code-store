8daeda6f60f46411badc8156a20c6654bdcc963e
Alexander Kuziv <makklays@gmail.com>
2021-08-27 09:43:53 +0200

    Create new-role-fullstack.php

---
+++ new-role-fullstack.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ * Author: Alexander Kuziv
+ * E-mail: makklays@gmail.com, alexander@makklays.com.ua
+ * Description: Looking for a new job FullStack Developer PHP
+ **/
+
+function newRole()
+{
+    return {
+        'position': 'FullStack Developer PHP',
+        'salary_min': '3000',
+        'salary_max': '5500',
+        'salary_currency': 'USD',
+        'location': 'office',
+        'notify': 'sms,email,phone'
+    }
+}
