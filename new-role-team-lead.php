3c9beb4047cd0450eaca3b23fc2f5b9e5f96de80
Alexander Kuziv <makklays@gmail.com>
2021-10-24 20:33:09 +0200

    Update new-role-team-lead.php

--- new-role-team-lead.php
+++ new-role-team-lead.php
@@ -5,15 +5,29 @@
  * Description: Looking for a new job Team Lead
  **/
 
-function newRoleTeamLead()
+interface DeveloperInterface
 {
-    return {
-        'position': 'Team Lead',
-        'salary_min': '2500',
-        'salary_max': '5000',
-        'salary_currency': 'USD',
-        'location': 'office',
-        'notify': 'sms,email,phone'
+    public function roleDeveloper   
+}
+
+interface TeamLeadInterface
+{
+    public function roleTeamLead
+}
+
+class TeamLead() implements DeveloperInterface, TeamLeadInterface
+{
+    function roleTeamLead()
+    {
+        return {
+            'position': 'Team Lead',
+            'salary_min': '2500',
+            'salary_max': '5000',
+            'salary_currency': 'USD',
+            'location': 'office',
+            'notify': 'sms,email,phone'
+        }
     }
 }
+
 ?>
