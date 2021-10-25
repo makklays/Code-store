d2cfaa9c8ce02290796d2901e9316f3f7e1b6b3f
Alexander Kuziv <makklays@gmail.com>
2021-10-25 19:38:11 +0200

    Update new-role.php

--- new-role.php
+++ new-role.php
@@ -5,15 +5,19 @@
  * Description: Looking for a new job Developer PHP
  **/
 
-function newRole()
+class NewRole
 {
-    return {
-        'position': 'Developer PHP',
-        'salary_min': '1500',
-        'salary_max': '4500',
-        'salary_currency': 'USD',
-        'location': 'office',
-        'notify': 'sms,email,phone'
+    function newRole()
+    {
+        return {
+            'position': 'Developer PHP',
+            'salary_min': '1500',
+            'salary_max': '4500',
+            'salary_currency': 'USD',
+            'location': 'office',
+            'notify': 'sms,email,phone'
+        }
     }
 }
+
 ?>
