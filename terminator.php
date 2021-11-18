1fccb259e6e22141b5fa1b1d46946cf3ef953bfc
Alexander Kuziv <makklays@gmail.com>
2021-11-18 21:00:51 +0100

    Create terminator.php

---
+++ terminator.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace mylife\office;
+
+class Terminator()
+{
+  public $name;
+  public $hand = 'up';
+  public $model = 'T700';
+  public $mission;
+  
+  public function create(): string
+  {
+    // 
+    return 'Created ' . date('d.m.Y');
+  }
+  
+  public function giveName(): string
+  {
+     return $this->name;
+  }
+  
+  public function setMission(string $mission): string
+  {
+    $this->mission = $mission; 
+  }
+  
+  public function getMission(): string
+  {
+    return $this->mission; 
+  }
+  
+  public function hand()
+  {
+    return $this->hand;  
+  }
+  
+  public function useWords()
+  {
+    $words = [
+      'delete',
+      'go to ass',
+      '',
+    ];
+    
+    return $words;  
+  }
+}
+
