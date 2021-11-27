e3fcf013ef20cde2fb2353bf652a9298d1bb65ea
Alexander Kuziv <makklays@gmail.com>
2021-11-27 21:47:05 +0100

    Create para-entravista.php

---
+++ para-entravista.php
@@ -0,0 +1,11 @@
+
+Imaginamos la entravista de PHP en español:
+
+- ¿Que es un objeto?
+- Un objeto es un instancia de clase
+
+- ¿Cuantas clases puede heredar otra clase en PHP? 
+- En PHP un clase puede heredar solo una clase
+
+- ¿Cuantas interfazes puede implementar una clase en PHP?
+- En PHP un clase puede implementar muchas interfazes
