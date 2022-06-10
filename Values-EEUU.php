3dc35fcec29e2ab6c5630060b35a86cba76132a5
Alexander Kuziv <makklays@gmail.com>
2022-06-10 13:00:19 +0200

    2022-06-10 Add values of EEUU

---
+++ Values-EEUU.php
@@ -0,0 +1,34 @@
+<?php
+namespace eeuu/ukraine;
+
+trait ValueEEUU()
+{
+  // Ценности ЕС (Европейского Союза)
+  return [
+      'ru' => [
+          '1' => 'уважение к человеческому достоинству',
+          '2' => 'уважение к правам человека',
+          '3' => 'свобода',
+          '4' => 'народовластие',
+          '5' => 'равноправие',
+          '6' => 'правовое государство',
+      ],
+      'es' => [
+          '1' => 'respeto de la dignidad humana',
+          '2' => 'respeto de los derechos humanos',
+          '3' => 'libertad',
+          '4' => 'poder popular',
+          '5' => 'igualdad',
+          '6' => 'estado de derecho',
+      ],
+      'en' => [
+          '1' => 'respect for human dignity',
+          '2' => 'respect for human rights',
+          '3' => 'freedom',
+          '4' => 'democracy',
+          '5' => 'equality',
+          '6' => 'rule of law',
+      ],
+  ];
+}
+
